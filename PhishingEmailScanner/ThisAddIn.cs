a754cfe67738b08534c6001f604f8d43be261141
Jakub Mikusek <j.mikusek01@gmail.com>
2025-11-25 20:08:58 +0100

    Dodaj pliki projektów.

---
+++ PhishingEmailScanner/ThisAddIn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Outlook = Microsoft.Office.Interop.Outlook;
+using Office = Microsoft.Office.Core;
+
+namespace PhishingEmailScanner
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            // Uwaga: program Outlook nie generuje już tego zdarzenia. Jeśli masz kod, który 
+            //    musi działać po zamknięciu programu Outlook, zobacz https://go.microsoft.com/fwlink/?LinkId=506785
+        }
+
+        #region Kod wygenerowany przez program VSTO
+
+        /// <summary>
+        /// Wymagana metoda obsługi projektanta — nie należy modyfikować 
+        /// zawartość tej metody z edytorem kodu.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
